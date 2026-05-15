c31fd37c1d2fc1951ad276c405ca988de9ac6819
Prince Narteh <prinarteh@gmail.com>
2026-05-15 13:31:29 +0000

    fix: code changes

--- backend/index.ts
+++ backend/index.ts
@@ -2,8 +2,13 @@ import app from "./src/app.ts";
 import { connectDB } from "./src/config/database.ts";
 
 const PORT = process.env.PORT || 3000;
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Failed to start the server: ", error);
+    process.exit(1);
   });
-});
--- backend/src/config/database.ts
+++ backend/src/config/database.ts
@@ -2,7 +2,11 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI as string, {
+    const mongoUri = process.env.MONGODB_URI;
+    if (!mongoUri) {
+      throw new Error("MONGODB_URI environment variable is not defined!");
+    }
+    await mongoose.connect(mongoUri, {
       dbName: process.env.MONGODB_DATABASE,
       user: process.env.MONGODB_USER,
       pass: process.env.MONGODB_PASS,
