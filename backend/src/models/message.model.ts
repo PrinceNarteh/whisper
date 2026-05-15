4c987093bd5955c63d99a5c6b72cee77e7c05c8e
Prince Narteh <prinarteh@gmail.com>
2026-05-15 12:54:54 +0000

    feat: add models

---
+++ backend/src/models/message.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Schema, type Document } from "mongoose";
+import { createPrinter } from "typescript";
+
+export interface IMessage extends Document {
+  chat: mongoose.Types.ObjectId;
+  sender: mongoose.Types.ObjectId;
+  text: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const MessageScheme = new Schema<IMessage>(
+  {
+    chat: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    sender: {
+      type: Schema.Types.ObjectId,
+      ref: "Chat",
+      required: true,
+    },
+    text: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+// indexes for faster queries
+MessageScheme.index({ chat: 1, createdAt: 1 });
+
+export const Message = mongoose.model("Message", MessageScheme);
